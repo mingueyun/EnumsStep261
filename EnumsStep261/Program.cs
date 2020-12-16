567f5d16af18b8ca6ca78f57e257add54524eb9b
student <student@learncodinganywhere.com>
2020-12-15 16:59:29 -0800

    Add project files.

---
+++ EnumsStep261/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsStep261
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weeks week = new Weeks();
+            week.DaysOfTheWeek = new DaysOfTheWeek();
+
+            Console.WriteLine("what day of the week is it? ");
+
+
+            bool input = false;
+            while (input == false)
+            {
+                try
+                {
+                    DaysOfTheWeek today = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine(), true);
+                    Console.WriteLine("You think today is: {0}", today.ToString());
+                    input = true;
+                }
+                catch (Exception )
+                {
+                    Console.WriteLine("Please enter an actual day of the week:");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ EnumsStep261/Weeks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsStep261
+{
+    class Weeks
+    {
+        public DaysOfTheWeek DaysOfTheWeek { get; set; }
+    }
+    public enum DaysOfTheWeek
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7,
+    }
+}
